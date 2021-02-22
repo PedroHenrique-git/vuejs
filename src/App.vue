d987f862fa9ba4f6dc002378333484b633d74d6a
PedroHenrique-git <39604283+PedroHenrique-git@users.noreply.github.com>
2021-02-22 17:00:34 -0300

    other tests

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <img alt='Vue logo' src='./assets/logo.png'>
   <HelloWorld msg='Welcome to Your Vue.js + TypeScript App'/>
   <Couter />
+  <Teste />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
 import Couter from './components/Couter.vue';
+import Teste from './components/Teste.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld,
     Couter,
+    Teste,
   },
 });
 </script>
---
+++ src/components/Teste.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="t" v-for="(pessoa, index) in list" v-bind:key="index">
+        <div v-bind="idade">{{ pessoa.nome }} | {{ pessoa.idade }}</div>
+    </div>
+    <h1>{{ message }}</h1>
+    <input type="text" v-model="message">
+    <button v-on:click="showMessage(message)">Click aqui</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+interface Myobject {
+    nome: string;
+    idade: number;
+}
+
+const arr: Array<Myobject> = [
+  { nome: 'Pedro', idade: 19 },
+  { nome: 'Luiz', idade: 21 },
+  { nome: 'Felipe', idade: 25 },
+];
+
+export default defineComponent({
+  name: 'Teste',
+  data() {
+    return {
+      list: arr,
+      message: 'Hello world',
+    };
+  },
+  methods: {
+    showMessage(msg: string) {
+      alert(msg);
+    },
+  },
+});
+
+</script>
+
+<style scoped>
+    .t {
+        background: green;
+    }
+    .t div {
+        color: black;
+    }
+</style>

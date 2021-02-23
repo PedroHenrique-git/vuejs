912f47e15febd49fc7b6125c6d8ee3d58853c174
PedroHenrique-git <39604283+PedroHenrique-git@users.noreply.github.com>
2021-02-23 14:52:29 -0300

    other examples

--- src/components/Teste.vue
+++ src/components/Teste.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="t" v-for="(pessoa, index) in list" v-bind:key="index">
-        <div v-bind="idade">{{ pessoa.nome }} | {{ pessoa.idade }}</div>
+        <div v-bind="idade" v-bind:id="index">{{ pessoa.nome }} | {{ pessoa.idade }}</div>
     </div>
     <h1>{{ message }}</h1>
     <input type="text" v-model="message">
+    <input type="text" v-model="person">
     <button v-on:click="showMessage(message)">Click aqui</button>
+    <button v-on:click="addPerson(person)">Click aqui para adicionar uma pessoa</button>
+    <form v-on:submit.prevent="onSubmit()">
+      <h1>Form</h1>
+      <input class="ip1" type="text" v-model="val">
+      <input type="submit" value="click here">
+    </form>
 </template>
 
 <script lang="ts">
@@ -27,11 +34,24 @@ export default defineComponent({
     return {
       list: arr,
       message: 'Hello world',
+      person: '',
+      val: '',
     };
   },
   methods: {
-    showMessage(msg: string) {
+    showMessage(msg: string): void {
       alert(msg);
+    },
+    addPerson(person: string): void {
+      const pessoa = person.split(' ');
+      this.list.push({ nome: pessoa[0], idade: Number(pessoa[1]) });
+    },
+    onSubmit() {
+      if (this.val !== '') {
+        alert('formulário enviado');
+        return;
+      }
+      alert('Preencha o campo');
     },
   },
 });
